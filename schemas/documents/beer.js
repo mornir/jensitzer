ac0d833d2940fdaa96d16b25e718edd5c796a66e
mornir <jeromepott@hotmail.com>
2021-02-06 21:50:49 +0100

    Fix number sorting

--- schemas/documents/beer.js
+++ schemas/documents/beer.js
@@ -98,7 +98,7 @@ export default {
     {
       title: 'Beer Number',
       name: 'beerNumber',
-      by: [{ field: 'number', direction: 'desc' }],
+      by: [{ field: 'number', direction: 'asc' }],
     },
   ],
 }
